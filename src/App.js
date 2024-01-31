9355267bf86fc46b25ccd989cbca08126621b270
F0xhopper <foxhopper16@gmail.com>
2024-01-31 11:34:17 +0000

    80% just need finsh ui

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import Generator from "./Components/generator";
 function App() {
   return (
     <div className="App">
+      {" "}
       <div className="titleContainer">
         <h1 className="title">Learn-Bot</h1>
-      </div>
-      <div className="titleContainer">
-        <h3>What do you want to learn about?</h3>
       </div>
       <Generator />
     </div>
--- src/Components/generator.js
+++ src/Components/generator.js
@@ -14,7 +14,7 @@ const Generator = () => {
   const [podcasts, setPodcasts] = useState();
   const [youtubeVideos, setYoutubeVideos] = useState();
   const [wikipediaArticles, setWikipediaArticles] = useState();
-
+  const [learnButtonText, setLearnButtonText] = useState("Learn");
   function setAllResourceUndefined() {
     setBooks(undefined);
     setDocumentaries(undefined);
@@ -30,6 +30,7 @@ const Generator = () => {
   };
   async function getResources() {
     setAllResourceUndefined();
+    setLearnButtonText("fetching....");
     for (const typeOf in state) {
       if (state[typeOf][0] == true) {
         await fetch("http://127.0.0.1:5000/post", {
@@ -46,6 +47,7 @@ const Generator = () => {
             return response.json();
           })
           .then((data) => {
+            console.log(data);
             if (typeOf == "bookCheck") {
               setBooks(data);
             } else if (typeOf == "documentariesCheck") {
@@ -57,130 +59,163 @@ const Generator = () => {
             } else if (typeOf == "wikipediaArticlesCheck") {
               setWikipediaArticles(data);
             }
-          })
-          .then(console.log(state));
+          });
       }
     }
+    setTopicInput("");
+    setLearnButtonText("Learn");
   }
   return (
     <div>
-      <div className="topicInputContainer">
-        <input
-          className="topicInput"
-          onChange={(e) => setTopicInput(e.target.value)}
-        ></input>
-      </div>
-
-      <div className="switchesContainer">
-        {" "}
-        <h5 className="howTitle">How do you want to learn About?</h5>
-        <FormControlLabel
-          control={
-            <Switch
-              color="default"
-              onChange={handleChange}
-              checked={state.bookCheck[0]}
-              name={"bookCheck"}
-            />
-          }
-          label="Books"
-          labelPlacement="top"
-        />
-        <FormControlLabel
-          control={
-            <Switch
-              color="default"
-              onChange={handleChange}
-              checked={state.documentariesCheck[0]}
-              name={"documentariesCheck"}
-            />
-          }
-          label="Documentaries"
-          labelPlacement="top"
-        />
-        <FormControlLabel
-          control={
-            <Switch
-              color="default"
-              onChange={handleChange}
-              checked={state.podcastsCheck[0]}
-              name={"podcastsCheck"}
-            />
-          }
-          label="Podcasts"
-          labelPlacement="top"
-        />
-        <FormControlLabel
-          control={
-            <Switch
-              color="default"
-              onChange={handleChange}
-              checked={state.youtubeVideosCheck[0]}
-              name={"youtubeVideosCheck"}
-            />
-          }
-          label="Youtube Videos"
-          labelPlacement="top"
-        />
-        <FormControlLabel
-          control={
-            <Switch
-              color="default"
-              onChange={handleChange}
-              checked={state.wikipediaArticlesCheck[0]}
-              name={"wikipediaArticlesCheck"}
-            />
-          }
-          label="Wiki Articles"
-          labelPlacement="top"
-        />
-      </div>
-      <div className="learnButtonContainer">
-        <button className="learnButton" onClick={getResources}>
-          Learn
-        </button>
-      </div>
-      <div className="mainResourceDisplayContainer">
-        {books ? (
-          <div className="mainDisplayContainer">
-            <h2>books</h2>
-            {books.map((book) => (
-              <p>{book}</p>
-            ))}
+      <div
+        style={{
+          marginTop:
+            books ||
+            documentaries ||
+            podcasts ||
+            youtubeVideos ||
+            wikipediaArticles
+              ? "0px"
+              : "300px",
+        }}
+        className="mainFunctionContainer"
+      >
+        <div className="topicInputContainer">
+          <div className="titleContainer">
+            <h4>What do you want to learn about?</h4>
           </div>
-        ) : null}
-        {documentaries ? (
-          <div className="mainDisplayContainer">
-            <h2>Dcumentaries</h2>
-            {documentaries.map((book) => (
-              <p>{book}</p>
-            ))}
+          <div className="topicInputContainer">
+            <input
+              value={topicInput}
+              className="topicInput"
+              onChange={(e) => setTopicInput(e.target.value)}
+            ></input>
           </div>
-        ) : null}
-        {podcasts ? (
-          <div className="mainDisplayContainer">
-            <h2>podcasts</h2>
-            {podcasts.map((book) => (
-              <p>{book}</p>
-            ))}
+        </div>
+        <div className="switchesContainer">
+          {" "}
+          {topicInput ? (
+            <h4 className="howTitle">
+              How do you want to learn about {topicInput}?
+            </h4>
+          ) : (
+            <h4 className="howTitle">How do you want to learn about it?</h4>
+          )}
+          <div className="switchContainer">
+            <FormControlLabel
+              control={
+                <Switch
+                  size="small"
+                  color="default"
+                  onChange={handleChange}
+                  checked={state.bookCheck[0]}
+                  name={"bookCheck"}
+                />
+              }
+              label="Books"
+              labelPlacement="top"
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  size="small"
+                  color="default"
+                  onChange={handleChange}
+                  checked={state.documentariesCheck[0]}
+                  name={"documentariesCheck"}
+                />
+              }
+              label="Documentaries"
+              labelPlacement="top"
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  size="small"
+                  color="default"
+                  onChange={handleChange}
+                  checked={state.podcastsCheck[0]}
+                  name={"podcastsCheck"}
+                />
+              }
+              label="Podcasts"
+              labelPlacement="top"
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  size="small"
+                  color="default"
+                  onChange={handleChange}
+                  checked={state.youtubeVideosCheck[0]}
+                  name={"youtubeVideosCheck"}
+                />
+              }
+              label="Youtube Videos"
+              labelPlacement="top"
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  size="small"
+                  color="default"
+                  onChange={handleChange}
+                  checked={state.wikipediaArticlesCheck[0]}
+                  name={"wikipediaArticlesCheck"}
+                />
+              }
+              label="Wiki Articles"
+              labelPlacement="top"
+            />
           </div>
-        ) : null}
-        {youtubeVideos ? (
-          <div className="mainDisplayContainer">
-            <h2>youtube Videos</h2>
-            {youtubeVideos.map((book) => (
-              <p>{book}</p>
-            ))}
-          </div>
-        ) : null}
-        {wikipediaArticles ? (
-          <div className="mainDisplayContainer">
-            <h2>wikipedia Articles</h2>
-            {wikipediaArticles.map((book) => (
-              <p>{book}</p>
-            ))}
-          </div>
-        ) : null}
+          <div className="learnButtonContainer">
+            <button className="learnButton" onClick={getResources}>
+              <h4>{learnButtonText}</h4>
+            </button>
+          </div>{" "}
+        </div>
+        <div className="mainResourceDisplayContainer">
+          {books ? (
+            <div className="mainDisplayContainer">
+              <h2>Books</h2>
+              {books.map((book) => (
+                <p>{book}</p>
+              ))}
+            </div>
+          ) : null}
+          {documentaries ? (
+            <div className="mainDisplayContainer">
+              <h2>Dcumentaries</h2>
+              {documentaries.map((book) => (
+                <p>{book}</p>
+              ))}
+            </div>
+          ) : null}
+          {podcasts ? (
+            <div className="mainDisplayContainer">
+              <h2>Podcasts</h2>
+              {podcasts.map((book) => (
+                <p>{book}</p>
+              ))}
+            </div>
+          ) : null}
+          {youtubeVideos ? (
+            <div className="mainDisplayContainer">
+              <h2>Youtube Videos</h2>
+              {youtubeVideos.map((book) => (
+                <p>{book}</p>
+              ))}
+            </div>
+          ) : null}
+          {wikipediaArticles ? (
+            <div className="mainDisplayContainer">
+              <h2>Wikipedia Articles</h2>
+              {wikipediaArticles.map((book) => (
+                <p>{book}</p>
+              ))}
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
