bac102ab32b78a4b1caea5193f271582c3e1182a
F0xhopper <foxhopper16@gmail.com>
2024-02-14 15:34:21 +0000

    sa

--- src/Components/generator.js
+++ src/Components/generator.js
@@ -49,7 +49,7 @@ const Generator = () => {
       });
 
       for (const typeOf in stateNew) {
-        await fetch("http://127.0.0.1:5000/post", {
+        await fetch("https://openai-api-uzyw.onrender.com/post", {
           method: "post",
 
           headers: {
